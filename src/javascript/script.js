3d3bdd40d321077bf11dadbaf211812e0ca91516
Juholiver <juholiver3@gmail.com>
2025-10-09 11:04:03 -0300

    first commit

---
+++ src/javascript/script.js
@@ -0,0 +1,18 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                const target = document.querySelector(this.getAttribute('href'));
+                if (target) {
+                    target.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    });
+                }
+            });
+        });
+
+        document.querySelector('.contact-form').addEventListener('submit', function(e) {
+            e.preventDefault();
+            alert('Mensagem enviada!');
+        });
