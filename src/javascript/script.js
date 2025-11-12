b83f87f1a32b2eeca7fe186c7315388371c2231b
Juholiver <juholiver3@gmail.com>
2025-11-12 14:38:15 -0300

    acionando background animado

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,18 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const animatedElements = document.querySelectorAll('.hero, .section, footer');
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-                const target = document.querySelector(this.getAttribute('href'));
-                if (target) {
-                    target.scrollIntoView({
-                        behavior: 'smooth',
-                        block: 'start'
-                    });
-                }
-            });
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in-element');
+                entry.target.classList.remove('hidden');
+                observer.unobserve(entry.target);
+            }
         });
+    }, {
+        threshold: 0.1
+    });
 
-        document.querySelector('.contact-form').addEventListener('submit', function(e) {
-            e.preventDefault();
-            alert('Mensagem enviada!');
-        });
+    animatedElements.forEach(element => {
+        element.classList.add('hidden');
+        observer.observe(element);
+    });
+});
+
+const nome = document.getElementById('nome').value;
+const mensagem = document.getElementById('mensagem').value;
+
+const telefone = "5515998985880";
+
+const texto = `Olá, meu nome é ${nome}, ${mensagem}`;
+const mensagemFormatada = encodeURIComponent(texto);
+
+function enviarMensagem() {
+    const url = `https://api.whatsapp.com/send?phone=${telefone}&text=${mensagemFormatada}`;
+    window.open(url, '_blank');
+}
